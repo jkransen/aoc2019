1166b22f65a3b981724d493ddb7f5e60a6b3d47e
Jeroen Kransen <jeroen@kransen.nl>
2019-12-14 11:35:27 +0100

    Day 14

--- src/main/scala/nl/kransen/aoc2019/Day14.scala
+++ src/main/scala/nl/kransen/aoc2019/Day14.scala
@@ -1,5 +1,87 @@
 package nl.kransen.aoc2019
 
-object Day14 {
+import scala.annotation.tailrec
+import scala.io.Source
 
+object Day14 extends App {
+
+  case class Ingredient(name: String, quantity: Long)
+
+  case class Recipe(target: Ingredient, sources: Set[Ingredient])
+
+  /**
+   * Converts a target ingredient into the sources needed to get it.
+   *
+   * @param target The target ingredient and amount to be produced
+   * @param recipes The recipes to go from ingredients to target.
+   * @return The ingredients needed to produce the desired amount of target
+   */
+  def unprocess(target: Ingredient, recipes: Map[String, Recipe]): Set[Ingredient] = {
+    val recipe = recipes(target.name)
+    val times = math.ceil(target.quantity.toFloat / recipe.target.quantity).toInt
+    recipe.sources.map(s => s.copy(quantity = s.quantity * times))
+  }
+
+  /**
+   * Merges two sets of Ingredients, so that duplicates are replaced by the summed amounts.
+   */
+  @tailrec
+  def merge(first: Set[Ingredient], second: Set[Ingredient]): Set[Ingredient] = {
+    if (first.isEmpty) {
+      second
+    } else {
+      val head = first.head
+      second.find(i => i.name.equals(head.name)) match {
+        case Some(s) => merge(first.tail, second - s + head.copy(quantity = head.quantity + s.quantity))
+        case None => merge(first.tail, second + head)
+      }
+    }
+  }
+
+  /**
+   * Recursively replaces the desired ingredients by the ingredients needed to get them, until only ORE remains.
+   */
+  @tailrec
+  def source(output: Set[Ingredient], recipes: Map[String, Recipe]): Set[Ingredient] = {
+    if (output.size == 1 && output.head.name == "ORE") {
+      output
+    } else {
+      val remainingRecipeInputs = recipes.values.flatMap(_.sources).map(_.name).toSet
+      val head = output.find(nxt => !remainingRecipeInputs.contains(nxt.name)).get
+      val neededForHead = unprocess(head, recipes)
+      source(merge(neededForHead, output - head), recipes - head.name)
+    }
+  }
+
+  def toIngredient(str: String): Ingredient = {
+    val chunks = str.trim.split(" ")
+    Ingredient(chunks(1), chunks(0).toInt)
+  }
+
+  def toRecipes(input: List[String]): Map[String, Recipe] = {
+    input.map { line =>
+      line.split("=>").toList match {
+        case sourcesCombined :: targetStr :: _ =>
+          val sourcesStr = sourcesCombined.split(",").toSet
+          val sources = sourcesStr.map(toIngredient)
+          val target = toIngredient(targetStr)
+          target.name -> Recipe(target, sources)
+      }
+    }.toMap
+  }
+
+  def source(): List[String] = {
+    val source = Source.fromResource("day14/input.txt")
+    val lines = source.getLines().toList
+    source.close()
+    lines
+  }
+
+  lazy val result1 = source(Set(Ingredient("FUEL", 1)), toRecipes(source()))
+  println(result1)
+
+  def oreForFuel(i: Long): Long = {
+    val ore = source(Set(Ingredient("FUEL", i)), toRecipes(source()))
+    ore.head.quantity
+  }
 }
--- src/test/scala/nl/kransen/aoc2019/Day14Spec.scala
+++ src/test/scala/nl/kransen/aoc2019/Day14Spec.scala
@@ -1,20 +1,42 @@
 package nl.kransen.aoc2019
 
+import nl.kransen.aoc2019.Day14.{Ingredient, Recipe}
 import org.scalatest._
 
-class Day2Spec extends FlatSpec with Matchers {
-  "The example from text" should "have output 3500" in {
-    val initial = Array(1,9,10,3,2,3,11,0,99,30,40,50)
-    val expected = Array(3500,9,10,70,2,3,11,0,99,30,40,50)
-    Day2.run(initial) shouldEqual expected
+class Day14Spec extends FlatSpec with Matchers {
+
+  "Example 1" should "finish with correct result" in {
+    val input = """
+      |10 ORE => 10 A
+      |1 ORE => 1 B
+      |7 A, 1 B => 1 C
+      |7 A, 1 C => 1 D
+      |7 A, 1 D => 1 E
+      |7 A, 1 E => 1 FUEL
+      |""".stripMargin.linesIterator.toList.tail
+    val recipes = Day14.toRecipes(input)
+    recipes shouldEqual Map("E" -> Recipe(Ingredient("E",1),Set(Ingredient("A",7), Ingredient("D",1))), "A" -> Recipe(Ingredient("A",10),Set(Ingredient("ORE",10))), "FUEL" -> Recipe(Ingredient("FUEL",1),Set(Ingredient("A",7), Ingredient("E",1))), "B" -> Recipe(Ingredient("B",1),Set(Ingredient("ORE",1))), "C" -> Recipe(Ingredient("C",1),Set(Ingredient("A",7), Ingredient("B",1))), "D" -> Recipe(Ingredient("D",1),Set(Ingredient("A",7), Ingredient("C",1))))
+    Day14.source(Set(Ingredient("FUEL", 1)), recipes) shouldEqual Set(Ingredient("ORE",31))
   }
 
-  "Part 1" should "finish with correct result" in {
-    Day2.run(Day2.initState())(0) shouldEqual 6627023
+  "Part 1" should "return the amount of ORE needed to produce 1 FUEL" in {
+    Day14.result1.head.quantity shouldBe 483766
+    Day14.oreForFuel(1) shouldBe 483766
   }
 
-  "Part 2" should "finish with correct result" in {
-    Day2.resultsPart2.headOption shouldEqual Some(4019)
-  }
+  "Part 2" should "return the greatest amount of FUEL needing < 1 trillion ORE" in {
+    val ore = 1000000000000L
+    ore / Day14.result1.head.quantity shouldBe 2067115
 
+    Day14.oreForFuel(11759443)   shouldBe 999999992087L
+    //                          val ore = 1000000000000L
+    Day14.oreForFuel(11759444)   shouldBe 1000000063895L
+
+    val i = for {
+      i <- LazyList.from(11758000, 1)
+      if Day14.oreForFuel(i) > 1000000000000L
+    } yield i - 1
+    println(i.head)
+    i.head shouldBe 11759443
+  }
 }
